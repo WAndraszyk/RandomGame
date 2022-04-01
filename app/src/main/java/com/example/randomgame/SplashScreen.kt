f2d103cb3307b0db6e47146ff1b7d35438c95fc0
Witold <wandraszyk73@gmail.com>
2022-04-01 20:06:33 +0200

    Handler

--- app/src/main/java/com/example/randomgame/SplashScreen.kt
+++ app/src/main/java/com/example/randomgame/SplashScreen.kt
@@ -1,17 +1,20 @@
 package com.example.randomgame
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
         supportActionBar?.hide()
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this@SplashScreen, HomeActivity::class.java)
             startActivity(intent)
         }, 3000)
