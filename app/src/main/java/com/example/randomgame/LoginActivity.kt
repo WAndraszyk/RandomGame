6d96d3bd5108453c34bb6929d1be884fdec9213b
Witold Andraszyk <wandraszyk73@gmail.com>
2022-04-06 10:30:37 +0200

    Pusty login activity

--- app/src/main/java/com/example/randomgame/LoginActivity.kt
+++ app/src/main/java/com/example/randomgame/LoginActivity.kt
@@ -1,14 +1,17 @@
 package com.example.randomgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        supportActionBar?.hide()
 
         val loginButton = findViewById<Button>(R.id.logIn)
         val nickField = findViewById<EditText>(R.id.nick)
@@ -17,6 +20,13 @@ class LoginActivity : AppCompatActivity() {
         loginButton.setOnClickListener {
             val nick = nickField.text.toString()
             val password = passwordField.text.toString()
+            val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+            startActivity(intent)
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(applicationContext, "LoginActivity on stop", Toast.LENGTH_LONG).show()
     }
 }
