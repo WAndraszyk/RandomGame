a6df088db1129e6418414a362d85350138c5b04f
Witold Andraszyk <wandraszyk73@gmail.com>
2022-04-06 14:46:13 +0200

    Poprawiona

--- app/src/main/java/com/example/randomgame/DBHelper.kt
+++ app/src/main/java/com/example/randomgame/DBHelper.kt
@@ -6,23 +6,24 @@ import android.database.sqlite.SQLiteOpenHelper
 
 class DBHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     companion object {
-        private const val DATABASE_VERSION = 2
+        private const val DATABASE_VERSION = 1
         private const val DATABASE_NAME = "GameDB.db"
-        //Table
-        private const val TABLE_NAME = "Players"
+        //Table Players
+        private const val TABLE_NAME1 = "Players"
         private const val COL_NICK = "Nick"
         private const val COL_PASS = "Password"
         private const val COL_LOGGED_IN = "Is_logged_in"
-        private const val COL_BEST_SCORE = "Key"
+        private const val COL_SCORE = "Score"
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        val createTableQuery = ("CREATE TABLE $TABLE_NAME ($COL_NICK VARCHAR PRIMARY KEY, $COL_PASS VARCHAR, $COL_LOGGED_IN CHAR, $COL_BEST_SCORE INTEGER)")
+        val createTableQuery = ("CREATE TABLE $TABLE_NAME1 ($COL_NICK VARCHAR PRIMARY KEY, $COL_PASS VARCHAR, $COL_LOGGED_IN CHAR, $COL_SCORE INTEGER)")
         db!!.execSQL(createTableQuery)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        db!!.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        val upgradeTableQuery = ("DROP TABLE IF EXISTS $TABLE_NAME1")
+        db!!.execSQL(upgradeTableQuery)
         onCreate(db)
     }
 }
